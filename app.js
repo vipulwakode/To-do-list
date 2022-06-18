64803c0877c8d9e44fd626c5699c616c76e3f257
vipulwakode <85163042+vipulwakode@users.noreply.github.com>
2022-06-18 14:28:59 +0530

    final

--- app.js
+++ app.js
@@ -113,10 +113,6 @@ app.post("/delete",function(req,res){
     }
    
 });
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port,function(){
-    console.log("Server started  on port 3000");
-});
+app.listen(process.env.PORT || 3000,function(){
+    console.log("Server is running on port 3000");
+})
